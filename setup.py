819ed444dd666a84723f382586fec46ca6606fc4
Feite Brekeveld <f.brekeveld@gmail.com>
2021-03-26 14:54:00 +0000

    chore(config): setup.py and requirements

--- setup.py
+++ setup.py
@@ -10,28 +10,29 @@ with open('README.rst') as readme_file:
 with open('CHANGELOG.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+# requirements = ['Click>=7.0', ]
+requirements = map(str.strip, open("requirements.txt").readlines())
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Feite Brekeveld",
     author_email='f.brekeveld@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="Virtual Finance API provides access to data from financial sites as if it was a REST-API.",
+    description="Virtual Finance API provides access to data from financial "
+                "sites as if it was a REST-API.",
     entry_points={
         'console_scripts': [
             'virtual_finance_api=virtual_finance_api.cli:main',
@@ -43,7 +44,8 @@ setup(
     include_package_data=True,
     keywords='virtual_finance_api',
     name='virtual_finance_api',
-    packages=find_packages(include=['virtual_finance_api', 'virtual_finance_api.*']),
+    packages=find_packages(include=['virtual_finance_api',
+                                    'virtual_finance_api.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
