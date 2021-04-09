80626904bd816f8356ab4de21ee741d756f775ba
Feite Brekeveld <f.brekeveld@gmail.com>
2021-04-09 10:58:29 +0000

    test(unittest): unittests regarding
    virtual_finance_api.extensions.stdjson
    virtual_finance_api.client

--- tests/test_client.py
+++ tests/test_client.py
@@ -52,8 +52,28 @@ class Simulate(VirtualAPIRequest):
         super(Simulate, self).__init__(endpoint, method=self.METHOD)
 
     def _conversion_hook(self, s):
+        # just to have some silly operation: reverse s 
         return s[::-1]
 
+
+@endpoint("some/{ticker}/thing", method="PUT", domain="https://test.com")
+class SimulatePUT(APIRequest):
+
+    def __init__(self, ticker, data):
+        endpoint = self.ENDPOINT.format(ticker=ticker)
+        super(SimulatePUT, self).__init__(endpoint, method=self.METHOD)
+        self.data = data
+
+
+@endpoint("some/{ticker}/thing", response_type='json', domain="https://test.com")
+class SimulateJSON(VirtualAPIRequest):
+
+    def __init__(self, ticker):
+        endpoint = self.ENDPOINT.format(ticker=ticker)
+        super(SimulateJSON, self).__init__(endpoint, method=self.METHOD)
+
+    def _conversion_hook(self, s):
+        return s
 
 @endpoint("some/{ticker}/thing", domain="https://test.com",
           response_type="json")
@@ -118,15 +138,62 @@ class TestClient(unittest.TestCase):
 
         self.assertEqual("{}".format(err.exception), text)
 
-    def test__requests001(self):
-        """expected status fetch."""
+    def test__request_status(self):
+        """expected status."""
         r = FakeVirtualRequest('IBM')
         self.assertTrue(r.expected_status == 200)
 
-    def test__requests002(self):
+    def test__request_chook(self):
         """conversion_hook."""
         r = FakeVirtualRequest('IBM')
         self.assertTrue(r._conversion_hook('abc') == 'abc')
+
+    @requests_mock.Mocker()
+    def test__data(self, mock_req):
+        """sending data."""
+        data = {'a': 10, 'b': 20}
+        r = SimulatePUT('IBM', data=data)
+        mock_req.register_uri('PUT',
+                              "{}/{}".format(API_URL, r),
+                              text="")
+        client.request(r)
+        self.assertTrue(r.expected_status == r.status_code and r.data == data)
+
+    @requests_mock.Mocker()
+    def test__404(self, mock_req):
+        """simulate a 404."""
+        data = {'a': 10, 'b': 20}
+        r = SimulatePUT('IBM', data=data)
+        mock_req.register_uri('PUT',
+                              "{}/{}".format(API_URL, r),
+                              status_code=404,
+                              text="")
+        with self.assertRaises(VirtualFinanceAPIError) as err:
+            client.request(r)
+        self.assertTrue(err.exception.code == 404)
+
+    @requests_mock.Mocker()
+    def test__json(self, mock_req):
+        """simulate a JSON endpoint."""
+        data = {'a': 10, 'b': 20}
+        r = SimulateJSON('IBM')
+        mock_req.register_uri('GET',
+                              "{}/{}".format(API_URL, r),
+                              text=json.dumps(data))
+        client.request(r)
+        self.assertTrue(r.response == data)
+
+    @requests_mock.Mocker()
+    def test__json_err(self, mock_req):
+        """simulate a JSON endpoint error."""
+        data = '{"a": 10, "b: 20}'  # error on purpose
+        r = SimulateJSON('IBM')
+        mock_req.register_uri('GET',
+                              "{}/{}".format(API_URL, r),
+                              text=data)
+        with self.assertRaises(ValueError) as err:
+            client.request(r)
+        self.assertTrue("response could not be loaded as JSON" in str(err.exception))
 
     @requests_mock.Mocker()
     def test__expected_status(self, mock_req):
@@ -175,7 +242,7 @@ class TestClient(unittest.TestCase):
 
     @requests_mock.Mocker()
     def test__conversion(self, mock_req):
-        """status change due to conversionhook error."""
+        """test__conversion."""
         indata = json.dumps({'tickerlist': ['IBM', 'CSCO']})
 
         r = SimulateStCh('IBM')
