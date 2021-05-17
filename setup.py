6170a1a05c2e2d70129d6d35865b872fec6210e4
Feite Brekeveld <f.brekeveld@gmail.com>
2021-05-17 15:45:35 +0000

    chore(setup.py): delete 'version=' from version

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/hootnot/virtual_finance_api",
-    version='version="0.4.2"',
+    version="0.4.2",
     zip_safe=False,
 )
